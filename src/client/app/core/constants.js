17ee2a6e95a8a686ecc7e70f07759112263736ca
Logan <lbarkes@ufl.edu>
2016-11-14 11:39:59 -0500

    Created a Movie-table2 directive: no search bar and replaced overview with release date

--- src/client/app/core/constants.js
+++ src/client/app/core/constants.js
@@ -6,5 +6,6 @@
     .module('app.core')
     .constant('toastr', toastr)
     .constant('moment', moment)
-    .constant('movieTableColumns', ['title', 'overview', 'rating_average', 'run_time']);
+    .constant('movieTableColumns', ['title', 'overview', 'rating_average', 'run_time'])
+	.constant('movieTableColumns2', ['title', 'release_date', 'rating_average', 'run_time']);
 })();
---
+++ src/client/app/widgets/movie-table2.directive.js
@@ -0,0 +1,43 @@
+(function() {
+  'use strict';
+
+  /**
+  *@desc a movie table directive that displays query results in a table with sortable rows and filter.
+  *@example <movie-table movies=vm.movies></movie-table>
+  */
+
+
+  angular
+    .module('app.widgets')
+    .directive('movieTable2', movieTable2);
+
+  /* @ngInject */
+  function movieTable2() {
+    var directive = {
+      restrict: 'E',
+      scope: {
+        movies: '='
+      },
+      controller: MovieTableController2,
+      controllerAs: 'vm',
+      bindToController: true,
+      templateUrl: 'app/widgets/movie-table2.html'
+    }
+
+    return directive;
+  }
+
+  MovieTableController.$inject = ['$scope', '$location', 'movieTableColumns2'];
+
+  function MovieTableController2 ($scope, $location, movieTableColumns2) {
+    var vm = this;
+    vm.movieTableColumns2 = movieTableColumns2;
+    vm.sortType = vm.movieTableColumns2[0];
+    vm.sortReverse = false;
+    vm.searchMovies;
+
+    vm.goToMovieDetail = function(movieId) {
+      $location.path('/movie/' + movieId);
+    }
+  }
+})();
