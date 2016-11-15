e7b35202377c0d62a5f3391c99f4f568f3ba28e9
chasefarmer2808 <chasefarmer2808@ufl.edu>
2016-11-14 20:38:37 -0500

    removing semicolon

--- src/client/app/core/constants.js
+++ src/client/app/core/constants.js
@@ -7,7 +7,7 @@
     .constant('toastr', toastr)
     .constant('moment', moment)
     .constant('movieTableColumns', ['title', 'overview', 'rating_average', 'run_time'])
-	  .constant('movieTableColumns2', ['title', 'release_date', 'rating_average', 'run_time']);
+	  .constant('movieTableColumns2', ['title', 'release_date', 'rating_average', 'run_time'])
     .constant('castTableColumns', ['name', 'role'])
     .constant('crewTableColumns', ['name', 'job', 'department']);
 })();
