daa0015913ff43aa78de726a14498cd47f09b042
chasefarmer2808 <chasefarmer2808@ufl.edu>
2016-11-16 00:18:39 -0500

    person detail view

--- src/client/app/person/person.controller.js
+++ src/client/app/person/person.controller.js
@@ -10,5 +10,22 @@
   function PersonController($q, logger, Oracle, $stateParams) {
     var vm = this;
     vm.personId = $stateParams.personId;
+    vm.person;
+
+    activate();
+
+    function activate() {
+      var promises = [getPerson()];
+      return $q.all(promises).then(function() {
+        logger.info('Activated Person Detail View');
+      });
+    }
+
+    function getPerson() {
+      return Oracle.getPersonById(vm.personId).then(function(data) {
+        vm.person = data.data;
+        console.log(vm.person);
+      });
+    }
   }
 })();
