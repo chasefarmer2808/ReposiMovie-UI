0d03c9f9fb8c468aab7ef5e4ca6b1de72449b58d
chasefarmer2808 <chasefarmer2808@ufl.edu>
2016-11-17 17:27:35 -0500

    main search functionality

--- src/client/app/mainsearch/mainsearch.controller.js
+++ src/client/app/mainsearch/mainsearch.controller.js
@@ -5,9 +5,18 @@
     .module('app.mainsearch')
     .controller('MainsearchController', MainsearchController);
 
-  MainsearchController.$inject = ['$q', 'logger', 'Oracle'];
+  MainsearchController.$inject = ['$q', 'logger', 'Oracle', 'movieTableColumns'];
   /*@ngInject*/
-  function MainsearchController($q, logger, Oracle) {
+  function MainsearchController($q, logger, Oracle, movieTableColumns) {
     var vm = this;
+    vm.search;
+    vm.movies;
+    vm.movieTableColumns = movieTableColumns;
+
+    vm.queryMovies = function() {
+      return Oracle.getMoviesByTitle(vm.search).then(function(data) {
+        vm.movies = data.data;
+      })
+    }
   }
 })();
