650840052913897c5e23e237d64c1a2236e51fee
chasefarmer2808 <chasefarmer2808@ufl.edu>
2016-11-21 18:50:49 -0500

    init user input model obj

--- src/client/app/advsearch/advsearch.controller.js
+++ src/client/app/advsearch/advsearch.controller.js
@@ -11,14 +11,45 @@
     var vm = this;
     vm.genres;
     vm.companies;
-    vm.cast;
-    vm.crew;
+    // vm.cast;
+    // vm.crew;
+    vm.minReleaseDate;
     vm.movies;
 
-    activate();
+    vm.queryParams = {
+      title: '',
+      startDate: new Date(vm.minReleaseDate),
+      endDate: new Date(),
+      avgRating: {
+        value: 0,
+        equality: '>'
+      },
+      ratingCount: {
+        value: 0,
+        equality: '>'
+      },
+      revenue: {
+        value: 0,
+        equality: '>'
+      },
+      budget: {
+        value: 0,
+        equality: '>'
+      },
+      runtime: {
+        value: 0,
+        equality: '>'
+      },
+      genres: [],
+      companies: [],
+      people: []
+    };
+
+
+    //activate();
 
     function activate() {
-      var promises = [getAllGenres(), getAllCompanies()];
+      var promises = [getAllGenres(), getAllCompanies(), getMinReleaseDate()];
       return $q.all(promises).then(function() {
         logger.info('Activated Adv Search View');
       });
@@ -36,16 +67,22 @@
       });
     }
 
-    function getAllCast() {
-      return Oracle.getAllCast().then(function(data) {
-        vm.cast = data.data;
+    function getMinReleaseDate() {
+      return Oracle.getMinReleaseDate().then(function(data) {
+        vm.minReleaseDate = data.data;
       });
     }
 
-    function getAllCrew() {
-      return Oracle.getAllCrew().then(function(data) {
-        vm.crew = data.data;
-      });
-    }
+    // function getAllCast() {
+    //   return Oracle.getAllCast().then(function(data) {
+    //     vm.cast = data.data;
+    //   });
+    // }
+    //
+    // function getAllCrew() {
+    //   return Oracle.getAllCrew().then(function(data) {
+    //     vm.crew = data.data;
+    //   });
+    // }
   }
 })();
