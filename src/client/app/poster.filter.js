80ded910210dc2c0e8204a4199f2674039ce2198
WilliamWickerson <willie7296@gmail.com>
2016-12-04 01:10:00 -0500

    Fixed broken photos

--- src/client/app/poster.filter.js
+++ src/client/app/poster.filter.js
@@ -2,6 +2,8 @@ angular.module('app').filter('poster', function() {
   return function(path) {
     if (!path)
       return 'images/not-found.jpg';
+    else if (path == "None")
+      return 'images/not-found.jpg';
     else
       return "http://image.tmdb.org/t/p/original" + path;
   }
